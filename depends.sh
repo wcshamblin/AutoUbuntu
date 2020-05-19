cbbc84c84ba69926a22a9afb0b67949d81d66eb9
wcshamblin <will@shamblin.org>
2020-05-19 10:58:04 -0400

    Initial commit

---
+++ depends.sh
@@ -0,0 +1,11 @@
+sudo apt-get install vim -y
+sudo apt-get install zsh -y
+sudo apt-get install curl -y
+sudo apt-get install git -y
+sudo apt-get install rxvt -y
+sudo apt-get install plasma-desktop -y
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+mv ./zshrc ~/.zshrc
+mv ./Xresources ~/.Xresources
+
