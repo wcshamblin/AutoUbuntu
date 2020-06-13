8b9b1d601c6cffea01d9fc2b7c8c28197eb19b08
wcshamblin <will@shamblin.org>
2020-06-13 09:57:04 -0400

    Added sublime text

--- depends.sh
+++ depends.sh
@@ -8,6 +8,7 @@ sudo apt-get install curl -y
 sudo apt-get install git -y
 sudo apt-get install rxvt -y
 sudo apt-get install plasma-desktop -y
+sudo apt-get install apt-transport-https -y
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 mv $DIR/zshrc ~/.zshrc
@@ -17,3 +18,7 @@ mkdir ~/.urxvt/ext/
 mv $DIR/urxvt/themes/* ~/.urxvt/themes/
 sudo cpan AnyEvent Linux::FD common::sense
 mv $DIR/urxvt/config/* ~/.urxvt/ext/
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text -y
